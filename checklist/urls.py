02191bb617887de731d6797d10f952536d9faeb6
Junho Ryu <jryu@users.noreply.github.com>
2016-01-15 14:45:18 -0800

    Fix Step 5
    Remove checklist_ prefix from URL name.

--- checklist/urls.py
+++ checklist/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from checklist import views
 
 urlpatterns = [
-    url(r'^$', views.Main.as_view(), name='checklist_main'),
+    url(r'^$', views.Main.as_view(), name='main'),
 ]
