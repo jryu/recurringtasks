e1f020bccde6372ee0fac30be777a5c17ac9beb3
Junho Ryu <jryu@users.noreply.github.com>
2016-02-12 23:56:38 -0800

    Step 19 - Translation
    http://recurringtasks.wordpress.com/2016/02/13/step-19-translation/

--- recurringtasks/settings_debug.py
+++ recurringtasks/settings_debug.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
